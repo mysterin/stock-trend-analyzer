305207848d276afa6f4552ffc434fb23d7cc134b
mysterin <mysterin@163.com>
2025-03-04 00:19:07 +0800

    增加定时任务

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from app.routers import stocks
+from app.scheduler.task import scheduler
+from contextlib import asynccontextmanager
 
 app = FastAPI()
 
 @app.get("/")
 async def home():
     return {"message": "home page"}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 在应用启动时启动调度器
+    scheduler.start()
+    yield
+    # 在应用关闭时关闭调度器
+    scheduler.shutdown()
+
+app = FastAPI(lifespan=lifespan)
 
 # 包含路由模块
 app.include_router(stocks.router)
