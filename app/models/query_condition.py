165b9ba78a4a56cc396ba642a426aaad422db7f6
mysterin <mysterin@163.com>
2025-03-14 19:52:51 +0800

    增加标题和备注

--- app/models/query_condition.py
+++ app/models/query_condition.py
@@ -11,5 +11,7 @@ class QueryCondition(Base):
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, comment='唯一标识符')
     page_name: Mapped[str] = mapped_column(String(50), comment='页面名称')
+    title: Mapped[Optional[str]] = mapped_column(String(100), comment='标题')
+    remark: Mapped[Optional[str]] = mapped_column(String(255), comment='说明')
     condition: Mapped[dict] = mapped_column(JSON, comment='查询条件')
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=text('CURRENT_TIMESTAMP'), comment='创建时间')
